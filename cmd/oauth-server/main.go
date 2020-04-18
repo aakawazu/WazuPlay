56f9c31527fa2cd440c4805e60af918006d9d0ec
aakawazu <aakawazu@protonmail.com>
2020-04-18 04:53:32 +0000

    godotenvを導入

--- cmd/oauth-server/main.go
+++ cmd/oauth-server/main.go
@@ -6,9 +6,14 @@ import (
 	"net/http"
 
 	oauthServer "github.com/aakawazu/WazuPlay/internal/oauth-server/router"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	err := godotenv.Load("../../.env")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
 	router := oauthServer.NewRouter()
 	fmt.Println("http://127.0.0.1:8080")
 	log.Fatal(http.ListenAndServe(":8080", router))
