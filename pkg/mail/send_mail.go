87f6bdb48471304f50be977f77d9eab21bc5b44b
aakawazu <aakawazu@protonmail.com>
2020-04-22 08:13:22 +0000

    いらないのを消した

--- pkg/mail/send_mail.go
+++ pkg/mail/send_mail.go
@@ -29,39 +29,5 @@ func Send(a Mail) error {
 		panic(err)
 	}
 
-	// servername := "mail:25"
-	// host, _, _ := net.SplitHostPort(servername)
-
-	// msg := "From: " + m.From + "\r\n" +
-	// 	"To: " + m.To + "\r\n" +
-	// 	"Subject: " + m.Subject + "\r\n\r\n" +
-	// 	m.Text + "\r\n"
-
-	// password := os.Getenv("SMTP_PASSWORD")
-	// auth := smtp.PlainAuth("", "wazuplay", password, host)
-
-	// client, err := smtp.Dial(servername)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// if err = client.Auth(auth); err != nil {
-	// 	log.Panic(err)
-	// }
-	// client.Mail(m.From)
-	// client.Rcpt(m.To)
-
-	// body, err := client.Data()
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// defer body.Close()
-
-	// buf := bytes.NewBufferString(msg)
-	// if _, err = buf.WriteTo(body); err != nil {
-	// 	log.Fatal(err)
-	// }
-
-	// client.Quit()
-
 	return nil
 }
