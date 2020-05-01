4fefaf1f483d1bfcd55549ba2dfb6f6a640fa04e
aakawazu <aakawazu@protonmail.com>
2020-05-01 04:49:49 +0000

    SMTPサーバーのパスワードを.envからもってくるようにした

--- pkg/mail/send_mail.go
+++ pkg/mail/send_mail.go
@@ -2,6 +2,7 @@ package mail
 
 import (
 	"crypto/tls"
+	"os"
 
 	"gopkg.in/gomail.v2"
 )
@@ -16,13 +17,14 @@ type Mail struct {
 
 //Send send mail
 func Send(a Mail) error {
+	password := os.Getenv("SMTP_PASSWORD")
 	m := gomail.NewMessage()
 	m.SetHeader("From", a.From)
 	m.SetHeader("To", a.To)
 	m.SetHeader("Subject", a.Subject)
 	m.SetBody("text/html", a.Text)
 
-	d := gomail.NewDialer("mail", 25, "wazuplay", "test")
+	d := gomail.NewDialer("mail", 25, "wazuplay", password)
 	d.TLSConfig = &tls.Config{InsecureSkipVerify: true}
 
 	if err := d.DialAndSend(m); err != nil {
