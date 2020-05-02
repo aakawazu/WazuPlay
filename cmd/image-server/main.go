86f6c528979f33fbc6b6b749888b298e17c6185c
aakawazu <aakawazu@protonmail.com>
2020-05-02 09:41:49 +0000

    画像サーバーを追加

--- cmd/image-server/main.go
+++ cmd/image-server/main.go
@@ -4,11 +4,17 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
+	imageServer "github.com/aakawazu/WazuPlay/internal/image-server/router"
+	"github.com/joho/godotenv"
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "hello, image-server")
-	})
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	err := godotenv.Load("../../.env")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	router := imageServer.NewRouter()
+	fmt.Println("hello, Image")
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
---
+++ internal/image-server/router/router.go
@@ -0,0 +1,20 @@
+package router
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+// NewRouter oauth server router
+func NewRouter() *mux.Router {
+	router := mux.NewRouter()
+	router.HandleFunc("/", Index)
+	return router
+}
+
+//Index index
+func Index(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, Image")
+}
