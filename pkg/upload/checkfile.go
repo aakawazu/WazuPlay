d185760407a89425a9b07534da627f24c523a4fa
aakawazu <aakawazu@protonmail.com>
2020-05-22 17:56:07 +0000

    いらなくなったのを消した消した

--- pkg/upload/checkfile.go
+++ pkg/upload/checkfile.go
@@ -2,12 +2,8 @@ package upload
 
 import (
 	"errors"
-	"fmt"
-	"io/ioutil"
 	"mime/multipart"
-	"os"
 
-	"github.com/aakawazu/WazuPlay/pkg/random"
 	"github.com/gabriel-vasile/mimetype"
 )
 
@@ -28,28 +24,4 @@ func CheckIfTheAllowedFileType(file multipart.File, allowedFiletype []string) er
 	}
 
 	return nil
-}
-
-// NumberOfFiles number of files
-func NumberOfFiles(rootFolder string, folderName string) (int, error) {
-	fileList, err := ioutil.ReadDir(fmt.Sprintf("%s/files/%s", rootFolder, folderName))
-	if err != nil {
-		return 0, err
-	}
-	return len(fileList), err
-}
-
-// CreateNewFolder create new folder
-func CreateNewFolder(rootFolder string) (string, error) {
-	newFolderName, err := random.GenerateRandomString()
-	if err != nil {
-		return "", err
-	}
-
-	err = os.MkdirAll(fmt.Sprintf("%s/files/%s", rootFolder, newFolderName), 0777)
-	if err != nil {
-		return "", err
-	}
-
-	return newFolderName, nil
 }
