77c9ab43ed4156dd92f3c8e369f082509f56a918
aakawazu <aakawazu@protonmail.com>
2020-05-23 01:46:49 +0000

    ファイルストレージからファイルを開けるようにした

---
+++ pkg/storage/open.go
@@ -0,0 +1,29 @@
+package storage
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/syndtr/goleveldb/leveldb"
+)
+
+// Open open file
+func Open(rootFolderURI string, fileName string) (*os.File, error) {
+	db, err := leveldb.OpenFile(fmt.Sprintf("%s/leveldb", rootFolderURI), nil)
+	if err != nil {
+		return nil, err
+	}
+	defer db.Close()
+
+	folderName, err := db.Get([]byte(fileName), nil)
+	if err != nil {
+		return nil, err
+	}
+
+	file, err := os.Open(fmt.Sprintf("%s/files/%s/%s", rootFolderURI, folderName, fileName))
+	if err != nil {
+		return nil, err
+	}
+
+	return file, nil
+}
