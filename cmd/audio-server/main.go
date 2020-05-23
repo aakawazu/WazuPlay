162cfbc164a5e8b7e80708619ec1a2a934cb6546
aakawazu <aakawazu@protonmail.com>
2020-05-23 06:41:41 +0000

    オーディオサーバーを初期化

--- cmd/audio-server/main.go
+++ cmd/audio-server/main.go
@@ -4,11 +4,21 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
+	audioServer "github.com/aakawazu/WazuPlay/internal/audio-server/router"
+	"github.com/aakawazu/WazuPlay/pkg/storage"
+	"github.com/joho/godotenv"
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "hello, audio-server")
-	})
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	err := godotenv.Load("../../.env")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	storage.Init(audioServer.AudioFilesRoot)
+
+	router := audioServer.NewRouter()
+	fmt.Println("hello, Audio")
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
