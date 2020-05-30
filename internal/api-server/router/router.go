d626108aee6d61afa095a6916d2d5d90f4e6e87c
aakawazu <aakawazu@protonmail.com>
2020-05-30 09:41:10 +0000

    ルーターを追加

---
+++ internal/api-server/router/router.go
@@ -0,0 +1,24 @@
+package router
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+// NewRouter api server router
+func NewRouter() *mux.Router {
+	router := mux.NewRouter()
+	router.HandleFunc("/", IndexHandler)
+	// router.HandleFunc("/album/register", RegisterAlbumHandler)
+	router.HandleFunc("/album/list", AlbumListHandler)
+	// router.HandleFunc("/album/{id}", AlbumInfoHandler)
+	// router.HandleFunc("/music/{id}", MusicInfoHandler)
+	return router
+}
+
+// IndexHandler /
+func IndexHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, WazuPlay")
+}
