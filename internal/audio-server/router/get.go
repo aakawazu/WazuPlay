c4772d44962c319984fbebc180e3a98fbdf879fe
aakawazu <aakawazu@protonmail.com>
2020-06-04 09:37:02 +0000

    音声ファイルを返す前にトークンを確認するようにした

--- internal/audio-server/router/get.go
+++ internal/audio-server/router/get.go
@@ -1,11 +1,15 @@
 package router
 
 import (
+	"fmt"
 	"io"
 	"net/http"
 
+	"github.com/aakawazu/WazuPlay/pkg/checkerr"
+	"github.com/aakawazu/WazuPlay/pkg/db"
 	"github.com/aakawazu/WazuPlay/pkg/httpstates"
 	"github.com/aakawazu/WazuPlay/pkg/storage"
+	"github.com/aakawazu/WazuPlay/pkg/token"
 	"github.com/gorilla/mux"
 	"github.com/syndtr/goleveldb/leveldb"
 )
@@ -17,7 +21,26 @@ func GetHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	accessToken := db.EscapeSinglequotation(r.URL.Query().Get("token"))
+	if accessToken == "" {
+		httpstates.BadRequest(&w)
+		return
+	}
+
+	userID, err := token.VerificationAccessToken(accessToken)
+
 	fileName := mux.Vars(r)["id"]
+
+	rows, err := db.RunSQL(fmt.Sprintf("SELECT FROM audio_files WHERE id = '%s' AND file_owner = '%s'", fileName, userID))
+
+	if checkerr.InternalServerError(&w, err) {
+		return
+	}
+
+	if !rows.Next() {
+		httpstates.NotFound(&w)
+		return
+	}
 
 	audioFile, err := storage.Open(AudioFilesRoot, fileName)
 
