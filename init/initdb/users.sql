95ed38994f184aded67d6610568dacd259f39f1a
aakawazu <aakawazu@protonmail.com>
2020-06-06 01:13:56 +0000

    DBテーブルを修正

--- init/initdb/users.sql
+++ init/initdb/users.sql
@@ -1,6 +1,6 @@
 create table users (
     user_id                     text,
-    username                    text,
+    user_name                   text,
     mail_address                text,
     hashed_password             text,
     profile_picture_url         text
